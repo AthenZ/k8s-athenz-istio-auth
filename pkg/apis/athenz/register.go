b6a7b35b1ec6f60ddcab5dafcbbe9d0747abec76
Michael Cieplak <mcieplak@verizonmedia.com>
2019-07-23 14:33:16 -0700

    Remove athenz domain cr, import athenz syncer

--- pkg/apis/athenz/register.go
+++
@@ -1,4 +0,0 @@
-// Copyright 2019, Verizon Media Inc.
-// Licensed under the terms of the 3-Clause BSD license. See LICENSE file in
-// github.com/yahoo/k8s-athenz-istio-auth for terms.
-package athenz
