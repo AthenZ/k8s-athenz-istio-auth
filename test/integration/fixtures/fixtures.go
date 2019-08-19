aae1b7622e0be42de13e1cc9fa3566c61156f126
Michael Cieplak <michael.cieplak@gmail.com>
2019-08-19 13:43:39 -0700

    Add initial integration test framework (#34)

---
+++ test/integration/fixtures/fixtures.go
@@ -0,0 +1,268 @@
+// Copyright 2019, Verizon Media Inc.
+// Licensed under the terms of the 3-Clause BSD license. See LICENSE file in
+// github.com/yahoo/k8s-athenz-istio-auth for terms.
+
+package fixtures
+
+import (
+	"log"
+
+	"github.com/ardielle/ardielle-go/rdl"
+	"github.com/yahoo/athenz/clients/go/zms"
+	athenzdomain "github.com/yahoo/k8s-athenz-syncer/pkg/apis/athenz/v1"
+	athenzdomainclientset "github.com/yahoo/k8s-athenz-syncer/pkg/client/clientset/versioned"
+
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// getAthenzDomainCrd returns the athenz domain custom resource definition
+func getAthenzDomainCrd() *v1beta1.CustomResourceDefinition {
+	return &v1beta1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "CustomResourceDefinition",
+			APIVersion: "apiextensions.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "athenzdomains.athenz.io",
+		},
+		Spec: v1beta1.CustomResourceDefinitionSpec{
+			Group: "athenz.io",
+			Scope: v1beta1.ClusterScoped,
+			Versions: []v1beta1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1",
+					Served:  true,
+					Storage: true,
+				},
+			},
+			Names: v1beta1.CustomResourceDefinitionNames{
+				Plural:     "athenzdomains",
+				Singular:   "athenzdomain",
+				Kind:       "AthenzDomain",
+				ShortNames: []string{"domain"},
+				ListKind:   "AthenzDomainList",
+			},
+		},
+	}
+}
+
+// getServiceRoleCrd returns the service role custom resource definition
+func getServiceRoleCrd() *v1beta1.CustomResourceDefinition {
+	return &v1beta1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "CustomResourceDefinition",
+			APIVersion: "apiextensions.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "serviceroles.rbac.istio.io",
+		},
+		Spec: v1beta1.CustomResourceDefinitionSpec{
+			Group: "rbac.istio.io",
+			Names: v1beta1.CustomResourceDefinitionNames{
+				Plural:   "serviceroles",
+				Singular: "servicerole",
+				Kind:     "ServiceRole",
+				Categories: []string{
+					"istio-io",
+					"rbac-istio-io",
+				},
+			},
+			Scope: v1beta1.NamespaceScoped,
+			Versions: []v1beta1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1alpha1",
+					Served:  true,
+					Storage: true,
+				},
+			},
+		},
+	}
+}
+
+// getServiceRoleBindingCrd returns the service role binding custom resource definition
+func getServiceRoleBindingCrd() *v1beta1.CustomResourceDefinition {
+	return &v1beta1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "CustomResourceDefinition",
+			APIVersion: "apiextensions.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "servicerolebindings.rbac.istio.io",
+		},
+		Spec: v1beta1.CustomResourceDefinitionSpec{
+			Group: "rbac.istio.io",
+			Names: v1beta1.CustomResourceDefinitionNames{
+				Plural:   "servicerolebindings",
+				Singular: "servicerolebinding",
+				Kind:     "ServiceRoleBinding",
+				Categories: []string{
+					"istio-io",
+					"rbac-istio-io",
+				},
+			},
+			Scope: v1beta1.NamespaceScoped,
+			Versions: []v1beta1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1alpha1",
+					Served:  true,
+					Storage: true,
+				},
+			},
+		},
+	}
+}
+
+// getClusterRbacConfigCrd returns the cluster rbac config custom resource definition
+func getClusterRbacConfigCrd() *v1beta1.CustomResourceDefinition {
+	return &v1beta1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "CustomResourceDefinition",
+			APIVersion: "apiextensions.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "clusterrbacconfigs.rbac.istio.io",
+		},
+		Spec: v1beta1.CustomResourceDefinitionSpec{
+			Group: "rbac.istio.io",
+			Names: v1beta1.CustomResourceDefinitionNames{
+				Plural:   "clusterrbacconfigs",
+				Singular: "clusterrbacconfig",
+				Kind:     "ClusterRbacConfig",
+				Categories: []string{
+					"istio-io",
+					"rbac-istio-io",
+				},
+			},
+			Scope: v1beta1.ClusterScoped,
+			Versions: []v1beta1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1alpha1",
+					Served:  true,
+					Storage: true,
+				},
+			},
+		},
+	}
+}
+
+// CreateCrds creates the athenz domain, service role, service role binding, and
+// cluster rbac config custom resource definitions
+func CreateCrds(clientset *apiextensionsclient.Clientset) error {
+	athenzDomainCrd := getAthenzDomainCrd()
+	serviceRoleCrd := getServiceRoleCrd()
+	serviceRoleBindingCrd := getServiceRoleBindingCrd()
+	clusterRbacConfigCrd := getClusterRbacConfigCrd()
+	crds := []*v1beta1.CustomResourceDefinition{athenzDomainCrd, serviceRoleCrd, serviceRoleBindingCrd, clusterRbacConfigCrd}
+
+	for _, crd := range crds {
+		_, err := clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// CreateAthenzDomain creates an athenz domain custom resource
+func CreateAthenzDomain(clientset athenzdomainclientset.Interface) {
+	domain := "home.foo"
+	fakeDomain := getFakeDomain()
+	newCR := &athenzdomain.AthenzDomain{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "AthenzDomain",
+			APIVersion: "athenz.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: domain,
+		},
+		Spec: athenzdomain.AthenzDomainSpec{
+			SignedDomain: fakeDomain,
+		},
+		Status: athenzdomain.AthenzDomainStatus{
+			Message: "",
+		},
+	}
+
+	list, err := clientset.AthenzV1().AthenzDomains().List(metav1.ListOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("list:", list)
+
+	created, err := clientset.AthenzV1().AthenzDomains().Create(newCR)
+	if err != nil {
+		log.Println("error creating athenz domain:", err)
+		return
+	}
+	log.Println("created cr:", created)
+
+	got, err := clientset.AthenzV1().AthenzDomains().Get(domain, metav1.GetOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("got cr:", got)
+}
+
+// getFakeDomain provides a populated fake domain object
+func getFakeDomain() zms.SignedDomain {
+	allow := zms.ALLOW
+	timestamp, err := rdl.TimestampParse("2019-06-21T19:28:09.305Z")
+	if err != nil {
+		panic(err)
+	}
+
+	domainName := "home.foo"
+	username := "user.foo"
+	return zms.SignedDomain{
+		Domain: &zms.DomainData{
+			Modified: timestamp,
+			Name:     zms.DomainName(domainName),
+			Policies: &zms.SignedPolicies{
+				Contents: &zms.DomainPolicies{
+					Domain: zms.DomainName(domainName),
+					Policies: []*zms.Policy{
+						{
+							Assertions: []*zms.Assertion{
+								{
+									Role:     domainName + ":role.admin",
+									Resource: domainName + ".test:*",
+									Action:   "*",
+									Effect:   &allow,
+								},
+							},
+							Modified: &timestamp,
+							Name:     zms.ResourceName(domainName + ":policy.admin"),
+						},
+					},
+				},
+				KeyId:     "col-env-1.1",
+				Signature: "signature-policy",
+			},
+			Roles: []*zms.Role{
+				{
+					Members:  []zms.MemberName{zms.MemberName(username)},
+					Modified: &timestamp,
+					Name:     zms.ResourceName(domainName + ":role.admin"),
+					RoleMembers: []*zms.RoleMember{
+						{
+							MemberName: zms.MemberName(username),
+						},
+					},
+				},
+				{
+					Trust:    "parent.domain",
+					Modified: &timestamp,
+					Name:     zms.ResourceName(domainName + ":role.trust"),
+				},
+			},
+			Services: []*zms.ServiceIdentity{},
+			Entities: []*zms.Entity{},
+		},
+		KeyId:     "colo-env-1.1",
+		Signature: "signature",
+	}
+}
---
+++ test/integration/framework/framework.go
@@ -0,0 +1,146 @@
+// Copyright 2019, Verizon Media Inc.
+// Licensed under the terms of the 3-Clause BSD license. See LICENSE file in
+// github.com/yahoo/k8s-athenz-istio-auth for terms.
+
+package framework
+
+import (
+	"io/ioutil"
+	"net"
+	"os"
+	"time"
+
+	"github.com/coreos/etcd/embed"
+	"github.com/yahoo/k8s-athenz-istio-auth/pkg/controller"
+	"github.com/yahoo/k8s-athenz-istio-auth/pkg/log"
+	"github.com/yahoo/k8s-athenz-istio-auth/test/integration/fixtures"
+	athenzdomainclientset "github.com/yahoo/k8s-athenz-syncer/pkg/client/clientset/versioned"
+
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubernetes/cmd/kube-apiserver/app"
+	"k8s.io/kubernetes/cmd/kube-apiserver/app/options"
+
+	"istio.io/istio/pilot/pkg/config/kube/crd"
+	"istio.io/istio/pilot/pkg/model"
+)
+
+type Framework struct {
+	K8sClientset          kubernetes.Interface
+	AthenzDomainClientset athenzdomainclientset.Interface
+	IstioClientset        *crd.Client
+	Controller            *controller.Controller
+	etcd                  *embed.Etcd
+	stopCh                chan struct{}
+}
+
+// runEtcd will setup up the etcd configuration and run the etcd server
+func runEtcd() (*embed.Etcd, error) {
+	etcdDataDir, err := ioutil.TempDir(os.TempDir(), "integration_test_etcd_data")
+	if err != nil {
+		return nil, err
+	}
+
+	config := embed.NewConfig()
+	config.Dir = etcdDataDir
+	return embed.StartEtcd(config)
+}
+
+// runApiServer will setup the api configuration and run the api server
+func runApiServer(certDir string) (*rest.Config, chan struct{}, error) {
+	s := options.NewServerRunOptions()
+
+	// TODO, remove the webhooks and api server certs
+	s.InsecureServing.BindAddress = net.ParseIP("127.0.0.1")
+	s.InsecureServing.BindPort = 8080
+	s.Etcd.StorageConfig.ServerList = []string{"http://127.0.0.1:2379"}
+	s.SecureServing.ServerCert.CertDirectory = certDir
+
+	completedOptions, err := app.Complete(s)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if errs := completedOptions.Validate(); len(errs) != 0 {
+		return nil, nil, errors.NewAggregate(errs)
+	}
+
+	stopCh := make(chan struct{})
+	server, err := app.CreateServerChain(completedOptions, stopCh)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	restConfig := &rest.Config{}
+	restConfig.Host = "http://127.0.0.1:8080"
+	return restConfig, stopCh, server.PrepareRun().NonBlockingRun(stopCh)
+}
+
+// Setup will run both etcd and api server together
+func Setup() (*Framework, error) {
+	etcd, err := runEtcd()
+	if err != nil {
+		return nil, err
+	}
+
+	restConfig, stopCh, err := runApiServer(etcd.Server.Cfg.DataDir)
+	if err != nil {
+		return nil, err
+	}
+
+	crdClientset, err := apiextensionsclient.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	err = fixtures.CreateCrds(crdClientset)
+	if err != nil {
+		return nil, err
+	}
+
+	k8sClientset, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	athenzDomainClientset, err := athenzdomainclientset.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	configDescriptor := model.ConfigDescriptor{
+		model.ServiceRole,
+		model.ServiceRoleBinding,
+		model.ClusterRbacConfig,
+	}
+
+	istioClientset, err := crd.NewClient("", "", configDescriptor, "svc.cluster.local")
+	if err != nil {
+		return nil, err
+	}
+
+	log.InitLogger("", "debug")
+	c := controller.NewController("svc.cluster.local", istioClientset, k8sClientset, athenzDomainClientset, time.Minute, time.Minute)
+	go c.Run(stopCh)
+
+	return &Framework{
+		K8sClientset:          k8sClientset,
+		AthenzDomainClientset: athenzDomainClientset,
+		IstioClientset:        istioClientset,
+		Controller:            c,
+		etcd:                  etcd,
+		stopCh:                stopCh,
+	}, nil
+}
+
+// Teardown will request the api server to shutdown
+func (f *Framework) Teardown() {
+	close(f.stopCh)
+	f.etcd.Close()
+	err := os.RemoveAll(f.etcd.Server.Cfg.DataDir)
+	if err != nil {
+		log.Println(err)
+	}
+}
