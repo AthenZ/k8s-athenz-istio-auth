b48648e875e99ec143880450e2bc34add0e20e4b
umothadaka <udaymadhav.mothadaka@yahooinc.com>
2024-08-22 13:28:43 -0500

    Cleanedup onboarding_test.go file
    Signed-off-by: umothadaka <udaymadhav.mothadaka@yahooinc.com>
    Signed-off-by: UdayMadhav88 <uday.madhav88@gmail.com>

--- test/integration/onboarding_test.go
+++ test/integration/onboarding_test.go
@@ -1,20 +1,14 @@
 package integration
 
 import (
-	// "istio.io/istio/pkg/config/schema/collections"
 	"testing"
-	//"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/yahoo/k8s-athenz-istio-auth/test/integration/fixtures"
 	"github.com/yahoo/k8s-athenz-istio-auth/test/integration/framework"
 
-	//"istio.io/api/rbac/v1alpha1"
-	// "istio.io/istio/pkg/config/constants"
-
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	//"k8s.io/apimachinery/pkg/util/wait"
 )
 
 // rolloutAndValidateOnboarding will create / update / delete / noop the service resource and wait for the
@@ -29,39 +23,6 @@ func rolloutAndValidateOnboarding(t *testing.T, s *fixtures.ExpectedServices, a 
 	case delete:
 		deleteServices(t, s)
 	}
-
-	// err := wait.PollImmediate(time.Second, time.Second*10, func() (bool, error) {
-	// 	config := framework.Global.IstioClientset.Get(collections.IstioRbacV1Alpha1Clusterrbacconfigs.Resource().GroupVersionKind(), constants.DefaultRbacConfigName, "")
-	// 	if config == nil && len(s.ServiceDNS) == 0 {
-	// 		return true, nil
-	// 	} else if config == nil {
-	// 		return false, nil
-	// 	}
-
-	// 	clusterRbacConfig, ok := config.Spec.(*v1alpha1.RbacConfig)
-	// 	if !ok {
-	// 		return false, nil
-	// 	}
-
-	// 	if len(clusterRbacConfig.Inclusion.Services) == len(s.ServiceDNS) {
-	// 		return true, nil
-	// 	}
-
-	// 	return false, nil
-	// })
-
-	// assert.Nil(t, err, "time out waiting for rollout for crc with error")
-
-	// crc := framework.Global.IstioClientset.Get(collections.IstioSecurityV1Beta1Authorizationpolicies.Resource().GroupVersionKind(), constants.DefaultRbacConfigName, "")
-	// if crc == nil && len(s.ServiceDNS) == 0 {
-	// 	return
-	// }
-
-	// clusterRbacConfig, ok := crc.Spec.(*v1alpha1.RbacConfig)
-	// assert.True(t, ok, "cluster rbac config cast should pass")
-	// assert.Equal(t, clusterRbacConfig.Mode, v1alpha1.RbacConfig_ON_WITH_INCLUSION, "cluster rbac config inclusion field should be set")
-	// assert.Nil(t, clusterRbacConfig.Exclusion, "cluster rbac config exclusion field should be nil")
-	// assert.ElementsMatch(t, s.ServiceDNS, clusterRbacConfig.Inclusion.Services, "cluster rbac config service list should be equal to expected")
 }
 
 // createServices will iterate through the service list and create each object
@@ -183,10 +144,6 @@ func TestDeleteCRC(t *testing.T) {
 func TestDeleteCRCIfServiceExists(t *testing.T) {
 	s := fixtures.GetExpectedServices(nil)
 	rolloutAndValidateOnboarding(t, s, create)
-
-	// err := framework.Global.IstioClientset.Delete(collections.IstioSecurityV1Beta1Authorizationpolicies.Resource().GroupVersionKind(), constants.DefaultRbacConfigName, "")
-	// assert.Nil(t, err, "cluster rbac config delete error should be nil")
-
 	rolloutAndValidateOnboarding(t, s, noop)
 	deleteServices(t, s)
 }
