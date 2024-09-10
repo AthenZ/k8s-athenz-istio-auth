4c6db29f545af8b9cda6b32b36c713f5d9ee7c37
UdayMadhav88 <uday.madhav88@gmail.com>
2024-09-10 18:53:41 -0500

    Increased apResyncInterval to 30 seconds
    Signed-off-by: UdayMadhav88 <uday.madhav88@gmail.com>

--- test/integration/framework/framework.go
+++ test/integration/framework/framework.go
@@ -167,7 +167,7 @@ func Setup() error {
 	serviceIndexInformer := cache.NewSharedIndexInformer(serviceListWatch, &v1.Service{}, 0, nil)
 	adIndexInformer := adInformer.NewAthenzDomainInformer(athenzDomainClientset, 0, cache.Indexers{})
 
-	apController := authzpolicy.NewController(configStoreCache, serviceIndexInformer, adIndexInformer, istioClientSet, time.Minute, enableOriginJwtSubject, componentsEnabledAuthzPolicy, combinationPolicyTag, authPolicyControllerOnlyMode, enableSpiffeTrustDomain, []string{"istio-system", "kube-yahoo"}, map[string]string{"istio-ingressgateway": "istio-system"}, []string{"k8s.omega.stage"})
+	apController := authzpolicy.NewController(configStoreCache, serviceIndexInformer, adIndexInformer, istioClientSet, 30*time.Second, enableOriginJwtSubject, componentsEnabledAuthzPolicy, combinationPolicyTag, authPolicyControllerOnlyMode, enableSpiffeTrustDomain, []string{"istio-system", "kube-yahoo"}, map[string]string{"istio-ingressgateway": "istio-system"}, []string{"k8s.omega.stage"})
 	go apController.Run(stopCh)
 
 	log.InitLogger("", "debug")
